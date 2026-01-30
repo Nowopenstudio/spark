6cf0f374aa8f62bf2fc1d443e3fcb4945d17a174
Ro Oeurn <ro@nowopen.studio>
2026-01-29 16:26:58 -0800

    new

--- app/components/three/threeScene.tsx
+++ app/components/three/threeScene.tsx
@@ -63,181 +63,181 @@ const Light = (props:any)=>{
   )
 }
 
-const Init =()=>{
+// const Init =()=>{
   
-  const source = '/texture/color.png'
-  const wMap = useLoader(TextureLoader, source)
+//   const source = '/texture/color.png'
+//   const wMap = useLoader(TextureLoader, source)
   
-  const tempCanvas = document.createElement("canvas");
+//   const tempCanvas = document.createElement("canvas");
   
-  tempCanvas.width = wMap.image.width;
-  tempCanvas.height = wMap.image.height;
+//   tempCanvas.width = wMap.image.width;
+//   tempCanvas.height = wMap.image.height;
   
-  const ctx = tempCanvas.getContext("2d");
-  ctx!.drawImage(wMap.image, 0, 0);
-  const imageData = ctx!.getImageData(0, 0, wMap.image.width, wMap.image.height);
+//   const ctx = tempCanvas.getContext("2d");
+//   ctx!.drawImage(wMap.image, 0, 0);
+//   const imageData = ctx!.getImageData(0, 0, wMap.image.width, wMap.image.height);
 
   
 
 
-  return(
+//   return(
 
-      <group>
-      <Dots imageData={imageData}/>
-    </group>
-
-
-  )
-}
+//       <group>
+//       <Dots imageData={imageData}/>
+//     </group>
 
 
-const Dots =({imageData}: any)=>{
-  const meshRef = useRef<any>(null)
-  const DOT_DENSITY = 32;
-  const RADIUS = 2.0
-  const LATITUDE_COUNT = 320
-  const dotCount=[]
-  const colorCount=[]
-  const dotGeometries = new Float32Array(56000*3)
-  const dotColor = new Float32Array(56000*3)
-  const vector = new THREE.Vector3();
-    const [currX,setX]=useState()
-  const [currY,setY]=useState()
-  const mousePosition = useRef({ x: 0, y: 0 });
-  const introProgress = useRef(100);
-
-  const updateMousePosition = useCallback((e) => {
-
-    mousePosition.current = { x: e.clientX, y: e.clientY };
-  }, []);
-// A hexagon with a radius of 2 pixels looks like a circle
+//   )
+// }
 
 
-  let currCount = 0
-  const texture = useTexture('/texture/color.png');
-  const color = useTexture('/texture/color.png');
+// const Dots =({imageData}: any)=>{
+//   const meshRef = useRef<any>(null)
+//   const DOT_DENSITY = 32;
+//   const RADIUS = 2.0
+//   const LATITUDE_COUNT = 320
+//   const dotCount=[]
+//   const colorCount=[]
+//   const dotGeometries = new Float32Array(56000*3)
+//   const dotColor = new Float32Array(56000*3)
+//   const vector = new THREE.Vector3();
+//     const [currX,setX]=useState()
+//   const [currY,setY]=useState()
+//   const mousePosition = useRef({ x: 0, y: 0 });
+//   const introProgress = useRef(100);
 
-  for (let lat = 0; lat < LATITUDE_COUNT;lat ++ ) {
-  const radius =Math.cos((-90 + (180 / LATITUDE_COUNT) * lat) * (Math.PI / 180)) * RADIUS;
-  const latitudeCircumference = radius * Math.PI * 2 * 2;
-  const latitudeDotCount = Math.ceil(latitudeCircumference * DOT_DENSITY);
+//   const updateMousePosition = useCallback((e) => {
+
+//     mousePosition.current = { x: e.clientX, y: e.clientY };
+//   }, []);
+// // A hexagon with a radius of 2 pixels looks like a circle
 
 
-  for (let dot = 0; dot < latitudeDotCount; dot += 1){
-    const dotGeometry = new THREE.CircleGeometry(2, 5);
-    const phi = (Math.PI / LATITUDE_COUNT) * lat;
-    const theta = ((2* Math.PI)/latitudeDotCount) * dot;
-      vector.setFromSphericalCoords(RADIUS, phi, theta);
+//   let currCount = 0
+//   const texture = useTexture('/texture/color.png');
+//   const color = useTexture('/texture/color.png');
+
+//   for (let lat = 0; lat < LATITUDE_COUNT;lat ++ ) {
+//   const radius =Math.cos((-90 + (180 / LATITUDE_COUNT) * lat) * (Math.PI / 180)) * RADIUS;
+//   const latitudeCircumference = radius * Math.PI * 2 * 2;
+//   const latitudeDotCount = Math.ceil(latitudeCircumference * DOT_DENSITY);
+
+
+//   for (let dot = 0; dot < latitudeDotCount; dot += 1){
+//     const dotGeometry = new THREE.CircleGeometry(2, 5);
+//     const phi = (Math.PI / LATITUDE_COUNT) * lat;
+//     const theta = ((2* Math.PI)/latitudeDotCount) * dot;
+//       vector.setFromSphericalCoords(RADIUS, phi, theta);
   
-      dotGeometry.lookAt(vector);
-      dotGeometry.translate(vector.x, vector.y, vector.z);
-      dotGeometry.computeBoundingSphere();
+//       dotGeometry.lookAt(vector);
+//       dotGeometry.translate(vector.x, vector.y, vector.z);
+//       dotGeometry.computeBoundingSphere();
 
-      const uv = spherePointToUV(
-        dotGeometry.boundingSphere!.center,
-        new THREE.Vector3()
-      );
-      const sampledPixel = sampleImage(imageData, uv);
+//       const uv = spherePointToUV(
+//         dotGeometry.boundingSphere!.center,
+//         new THREE.Vector3()
+//       );
+//       const sampledPixel = sampleImage(imageData, uv);
 
       
 
-      if (sampledPixel[3]) {
-        // Push the positioned geometry into the array.
-        dotGeometries.set([vector.x, vector.y, vector.z], (currCount * 3));
-        dotColor.set([sampledPixel[0]/255, sampledPixel[1]/255, sampledPixel[2]/255], (currCount * 3));
-        dotCount.push(dotGeometry)
-        colorCount.push(dotColor)
-        currCount += 1
-      }
+//       if (sampledPixel[3]) {
+//         // Push the positioned geometry into the array.
+//         dotGeometries.set([vector.x, vector.y, vector.z], (currCount * 3));
+//         dotColor.set([sampledPixel[0]/255, sampledPixel[1]/255, sampledPixel[2]/255], (currCount * 3));
+//         dotCount.push(dotGeometry)
+//         colorCount.push(dotColor)
+//         currCount += 1
+//       }
 
-      // dotGeometries.set([vector.x, vector.y, vector.z], (currCount * 3));
-      // dotCount.push(dotGeometry)
+//       // dotGeometries.set([vector.x, vector.y, vector.z], (currCount * 3));
+//       // dotCount.push(dotGeometry)
 
       
 
-  }
+//   }
  
    
-}
+// }
+// //   useEffect(() => {
+
+// //   sprite.colorSpace = SRGBColorSpace
+// // }, [])
+
+//   const uniforms = useMemo(() => ({
+//     uTime: {
+//       value: 0.0
+//     },
+//     uTexture:{
+//       value:color
+//     },
+//     uProgress: {
+//       value: 8.0
+//     },
+//     uMouse: { value: new Vector2(0, 0) },
+//     uScreen: { value: new Vector2(window.innerWidth/2, window.innerHeight/2) },
+//   }), [])
 //   useEffect(() => {
+//     window.addEventListener("mousemove", updateMousePosition, false);
 
-//   sprite.colorSpace = SRGBColorSpace
-// }, [])
-
-  const uniforms = useMemo(() => ({
-    uTime: {
-      value: 0.0
-    },
-    uTexture:{
-      value:color
-    },
-    uProgress: {
-      value: 8.0
-    },
-    uMouse: { value: new Vector2(0, 0) },
-    uScreen: { value: new Vector2(window.innerWidth/2, window.innerHeight/2) },
-  }), [])
-  useEffect(() => {
-    window.addEventListener("mousemove", updateMousePosition, false);
-
-    return () => {
-      window.removeEventListener("mousemove", updateMousePosition, false);
-    };
-  }, [updateMousePosition]);
+//     return () => {
+//       window.removeEventListener("mousemove", updateMousePosition, false);
+//     };
+//   }, [updateMousePosition]);
 
 
-  useFrame((state, delta) => {
-    const { clock, mouse} = state;
+//   useFrame((state, delta) => {
+//     const { clock, mouse} = state;
     
   
-       meshRef.current.material.uniforms.uTime.value = clock.elapsedTime;
-    if(clock.elapsedTime < 3 )
-      {meshRef.current.material.uniforms.uProgress.value = clock.elapsedTime - 3}
-    meshRef.current.material.uniforms.uMouse.value = new Vector2(
-    );
-  // for (let i = 0; i < currCount; i++) {
-  //     const i3 = i * 3;
-  //     meshRef.current!.geometry.attributes.position.array[i3] += Math.sin(clock.elapsedTime + Math.random() * 10) * 0.0001;
-  //     // meshRef.current!.geometry.attributes.position.array[i3 + 1] += Math.cos(clock.elapsedTime + Math.random() * 10) * 0.0001;
-  //     // meshRef.current!.geometry.attributes.position.array[i3 + 2] += Math.sin(clock.elapsedTime + Math.random() * 20) * 0.0001;
-  //   }
+//        meshRef.current.material.uniforms.uTime.value = clock.elapsedTime;
+//     if(clock.elapsedTime < 3 )
+//       {meshRef.current.material.uniforms.uProgress.value = clock.elapsedTime - 3}
+//     meshRef.current.material.uniforms.uMouse.value = new Vector2(
+//     );
+//   // for (let i = 0; i < currCount; i++) {
+//   //     const i3 = i * 3;
+//   //     meshRef.current!.geometry.attributes.position.array[i3] += Math.sin(clock.elapsedTime + Math.random() * 10) * 0.0001;
+//   //     // meshRef.current!.geometry.attributes.position.array[i3 + 1] += Math.cos(clock.elapsedTime + Math.random() * 10) * 0.0001;
+//   //     // meshRef.current!.geometry.attributes.position.array[i3 + 2] += Math.sin(clock.elapsedTime + Math.random() * 20) * 0.0001;
+//   //   }
     
-    meshRef.current!.geometry.attributes.position.needsUpdate = true;
-    meshRef.current!.geometry.attributes.color.needsUpdate = true;
-    meshRef.current!.rotation.y += (delta*.1)
-  });
+//     meshRef.current!.geometry.attributes.position.needsUpdate = true;
+//     meshRef.current!.geometry.attributes.color.needsUpdate = true;
+//     meshRef.current!.rotation.y += (delta*.1)
+//   });
 
 
-  return (
+//   return (
       
-  <points ref={meshRef} position={[0,0,0]} scale={1.2}>
-    <bufferGeometry>
-      <bufferAttribute
-        attach={"attributes-position"}
-        array={dotGeometries}
-        count={dotGeometries.length/3}
-        itemSize={3}
-      />
-       <bufferAttribute
-        attach={"attributes-color"}
-        array={dotColor}
-        count={dotColor.length/3}
-        itemSize={3}
-      />
-    </bufferGeometry>
-    <shaderMaterial
-          fragmentShader={fragmentShader}
-          vertexShader={vertexShader}
-          uniforms={uniforms}
-          transparent={true}
-          depthWrite={false}
-          depthTest={true}
-        />
+//   <points ref={meshRef} position={[0,0,0]} scale={1.2}>
+//     <bufferGeometry>
+//       <bufferAttribute
+//         attach={"attributes-position"}
+//         array={dotGeometries}
+//         count={dotGeometries.length/3}
+//         itemSize={3}
+//       />
+//        <bufferAttribute
+//         attach={"attributes-color"}
+//         array={dotColor}
+//         count={dotColor.length/3}
+//         itemSize={3}
+//       />
+//     </bufferGeometry>
+//     <shaderMaterial
+//           fragmentShader={fragmentShader}
+//           vertexShader={vertexShader}
+//           uniforms={uniforms}
+//           transparent={true}
+//           depthWrite={false}
+//           depthTest={true}
+//         />
     
-  </points>
+//   </points>
  
-  )
-}
+//   )
+// }
 
 
 
@@ -261,7 +261,7 @@ export function Test() {
                   <Lightformer color="white" intensity={0.2} onUpdate={(self) => self.lookAt(0, 0, 0)} position={[-20, 1, 0]} scale={[20, 100, 1]} />
                 </group>
               </Environment>
-              <Init />
+              {/* <Init /> */}
           
               <Logo/>
         
